f0e74cfb02afdb74eb63ca5ace9cc025655e03f5
Malak <benmalak658@gmail.com>
2023-12-20 11:04:13 -0800

    Commit message

--- voting/src/SingingContest.sol
+++ voting/src/SingingContest.sol
@@ -2,30 +2,62 @@
 pragma solidity ^0.8.19;
 
 contract SingingContest {
-    mapping (bytes32 => uint8) public votesReceived;
-    bytes32[] public singerList;
+    // Struct to represent a singer with a name and total votes received
+    struct Singer {
+        bytes32 name;
+        uint8 votesReceived;
+    }
 
+    // Array of Singer structs to store information about each singer
+    Singer[] public singers;
+
+    // Mapping to check if a singer name is valid
+    mapping (bytes32 => bool) public validSingers;
+
+    // Event to notify when a vote is cast
+    event Voted(bytes32 indexed singer, address indexed voter);
+
+    // Constructor to initialize the list of singers
     constructor(bytes32[] memory singerNames) {
-        singerList = singerNames;
+        require(singerNames.length > 0, "Singer list cannot be empty");
+        
+        for (uint i = 0; i < singerNames.length; i++) {
+            require(!validSingers[singerNames[i]], "Duplicate singer names are not allowed");
+            singers.push(Singer(singerNames[i], 0));
+            validSingers[singerNames[i]] = true;
+        }
     }
 
-    function totalVotesFor(bytes32 singer) public view returns (uint8) {
+    // Function to get the total votes for a singer
+    function totalVotesFor(bytes32 singer) external view returns (uint8) {
         require(validSinger(singer), "Invalid singer");
-        return votesReceived[singer];
+        return votesReceived(singer);
     }
 
-    function voteForSinger(bytes32 singer) public {
+    // Function to vote for a singer
+    function voteForSinger(bytes32 singer) external {
         require(validSinger(singer), "Invalid singer");
-        votesReceived[singer] += 1;
+        singers[_singerIndex(singer)].votesReceived += 1;
+        emit Voted(singer, msg.sender);
     }
 
+    // Function to check if a singer is valid
     function validSinger(bytes32 singer) public view returns (bool) {
-        for(uint i = 0; i < singerList.length; i++) {
-            if (singerList[i] == singer) {
-                return true;
+        return validSingers[singer];
+    }
+
+    // Internal function to get the index of a singer in the singers array
+    function _singerIndex(bytes32 singer) internal view returns (uint) {
+        for (uint i = 0; i < singers.length; i++) {
+            if (singers[i].name == singer) {
+                return i;
             }
         }
-        return false;
+        revert("Singer not found");
+    }
+
+    // Internal function to get the votes received for a singer
+    function votesReceived(bytes32 singer) internal view returns (uint8) {
+        return singers[_singerIndex(singer)].votesReceived;
     }
 }
-
